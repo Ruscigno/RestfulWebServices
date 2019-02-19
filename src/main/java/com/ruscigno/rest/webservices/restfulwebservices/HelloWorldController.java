2874e7a0fb212a390d56c1ca62b5591b6ab9109b
Sander Ruscigno <ruscigno@gmail.com>
2019-02-18 21:15:05 -0300

    Add Hello World Bean Class Controller

--- src/main/java/com/ruscigno/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/ruscigno/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -8,10 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-
-	//@RequestMapping(method=RequestMethod.GET, path="/hello-world")
-	@GetMapping(path="/hello-world")
+	// @RequestMapping(method=RequestMethod.GET, path="/hello-world")
+	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}// @RequestMapping(method=RequestMethod.GET, path="/hello-world")
+
+	// hello-world-bean
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World Bean");
 	}
 }
